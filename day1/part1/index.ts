6286a16f4675fdbaaf6b1eee27589e8f1f7b3bd5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-19 14:17:14 +0100

    shortened the solution a bit

--- day1/part1/index.ts
+++ day1/part1/index.ts
@@ -18,22 +18,6 @@
 import * as FileSystem from "fs/promises";
 import * as Process from "process";
 
-const parseIntOr = (fallback: number, input: unknown): number => {
-  const parsedInt: number = (() => {
-    if (typeof input === "string") {
-      return parseInt(input, 10);
-    }
-
-    return parseInt(String(input), 10);
-  })();
-
-  if (Number.isNaN(parsedInt)) {
-    return fallback;
-  }
-
-  return parsedInt;
-};
-
 const main = async () => {
   const path = Process.argv[2];
 
@@ -41,54 +25,34 @@ const main = async () => {
     throw new Error("A path must be provided\nexample: npm --workspace day1/part1 test input.txt");
   }
 
-  const buffer: Buffer = await FileSystem.readFile(path);
+  const buffer = await FileSystem.readFile(path);
 
-  const text: string = buffer.toString();
-
-  const lines: Array<string> = text.split("\n");
-
-  const linesWithoutEmptyLines = lines.filter(line => {
-    return line.trim().length !== 0;
-  });
-
-  const linesWithCharacters: Array<Array<string>> = linesWithoutEmptyLines.map(line => {
-    return line.split("");
-  });
-
-  const linesWithNumbers: Array<Array<number>> = linesWithCharacters.map(lineWithCharacters => {
-    return lineWithCharacters.map(character => {
-      return parseInt(character, 10);
-    }).filter(number => {
-      return !Number.isNaN(number);
+  const solution = buffer.toString().split("\n").filter(Boolean).map(line => {
+    return line.split("").map(character => {
+      return parseInt(character);
+    }).filter((digitOrNotANumber) => {
+      return !Number.isNaN(digitOrNotANumber);
     });
-  });
+  }).map((digits) => {
+    const firstDigit = digits.at(0);
+    const lastDigit = digits.at(-1);
 
-  const linesWithCalibration: Array<number> = linesWithNumbers.map(lineWithNumbers => {
-    const firstNumber: number | undefined = lineWithNumbers.at(0);
-    const lastNumber: number | undefined = lineWithNumbers.at(-1);
-
-    if (typeof firstNumber === "undefined") {
+    if (firstDigit === undefined) {
       return 0;
     }
 
-    if (typeof lastNumber === "undefined") {
-      return parseIntOr(0, `${firstNumber}`.repeat(2));
+    if (lastDigit === undefined) {
+      return parseInt(firstDigit.toString().repeat(2));
     }
 
-    return parseIntOr(0, `${firstNumber}${lastNumber}`);
-  });
-
-  const solution: number = linesWithCalibration.reduce((sum, calibration) => {
-    return sum + calibration;
+    return parseInt(`${firstDigit}${lastDigit}`);
+  }).reduce((sum, digit) => {
+    return sum + digit;
   }, 0);
 
   console.log(solution);
 };
 
 main().catch(error => {
-  if (error instanceof Error) {
-    console.error(error.message);
-  } else {
-    console.error(String(error));
-  }
+  console.error(`An error occurred: ${error}`);
 });
