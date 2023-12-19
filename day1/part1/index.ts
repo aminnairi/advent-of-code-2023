992bdd2e5682968a8a7f13f23a5f3a84268f1c53
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-12-19 11:49:53 +0100

    added gnu gpl v3 license header

--- day1/part1/index.ts
+++ day1/part1/index.ts
@@ -1,3 +1,20 @@
+/**
+ * advent-of-code-2023
+ * Copyright (C) 2023 Amin NAIRI
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import * as FileSystem from "fs/promises";
 import * as Process from "process";
 
